634b0c7c7db6846cd112fa933abb59c8dd2420f3
replay <replaykim123@gmail.com>
2016-06-24 20:13:00 +0900

    exam

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,39 @@
-console.log('my-note app.js');
+(function($, app){
+
+	console.log('my-note app.js');
+	var $header = $('.header');
+	var $memo = $('#memo')
+	var initData = app.util.storage.load();
+
+	
+	
+	$header.on('click', '.btn-fullscreen', function(event){
+
+		app.BtnAction.fullScreen();
+
+	});
+
+	$header.on('click','.btn-about' , function(){
+
+		app.BtnAction.about();
+	});
+
+	$header.on('click','.btn-savenote' ,function(){
+
+		app.view.addTodo();
+
+	});
+
+	$header.on('click', '.btn-newnote' ,function(){
+		$memo.val('');
+		
+	});
+
+
+	if(initData.length >= 1) {
+		console.log(initData)
+		// app.collection.set( initData );
+		$memo.val(initData[(initData.length-1)].value)
+	}
+
+})(jQuery, NoteApp);
